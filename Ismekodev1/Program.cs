58e5119d3e65a9df83bc116e6b7dc617c4149868
melikealara <117519618+melikealara@users.noreply.github.com>
2024-02-28 21:25:24 +0300

    .

---
+++ Ismekodev1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Ismekodev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Suyun sıcaklık değeri dirildiğinde suyun sıcaklığına göre maddenin halini(katı, sıvı, gaz)
+               gösteren program. Not: suyun kaynama ısısı 100 donma ısısı 0 derece 
+             */
+
+            //0 ve 100 derecede su iki halde de bulunabiliyor ama ben sıvı olarak kabul ettim.
+
+            int degree = Convert.ToInt32(Console.ReadLine());
+
+            if (degree > 0)
+            {
+                Console.WriteLine("katı");
+            }
+            else if (degree < 0)
+            {
+                Console.WriteLine("gaz");
+            }
+            else
+            {
+                Console.WriteLine("sıvı");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
